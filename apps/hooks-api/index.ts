cc64c97d562da703ae571c33ed7006523f1f5350
mohd kaif <mk2818356@gmail.com>
2026-04-09 03:42:10 +0530

    added db

--- apps/hooks-api/index.ts
+++ apps/hooks-api/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { prisma } from "@repo/db/client"
+import express from "express"
+
+const app = express()
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res)=>{
+    const { userId, zapId } = req.params
+    
+    const resposne = prisma.$transaction(async (tx) =>{
+        const run = await prisma.zapRun.create({
+            data : {
+                zapId : zapId
+            }
+        })
+        await prisma.zapRunOutbox.create({
+            data : {
+                zapRunId : run.id
+            }
+        })
+    })
+})
